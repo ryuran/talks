01870eed3ef840d5bafb1eebcc6cc6c8aff1bdb8
Yvain Liechti <yliechti@clever-age.com>
2018-11-25 21:25:42 +0100

    fix root url

--- tools/twig/index.js
+++ tools/twig/index.js
@@ -1,5 +1,6 @@
 const twig = require('gulp-twig-pipe');
-const dateFns = require('date-fns')
+const dateFns = require('date-fns');
+var path = require('path');
 
 twig.extend(Twig => {
   Twig.exports.extendFilter('date', function (date, format, lang) {
@@ -7,6 +8,32 @@ twig.extend(Twig => {
   });
   Twig.exports.extendFilter('isToday', function (date) {
     return dateFns.isToday(new Date(date))
+  });
+
+  Twig.exports.extendTag({
+    type : 'root',
+    regex: /^root$/,
+    next : [ ],
+    open : true,
+
+    compile: function (token) {
+      delete token.match;
+      return token;
+    },
+
+    parse: function (token, context/*, chain*/) {
+      const dir   = path.parse(context._target.relative).dir;
+      let depth = (dir === '' ? [] : dir.split(path.sep)).length;
+
+      return {
+        chain : false,
+        output: (function () {
+          const up = ['.'];
+          while (depth--) { up.push('..'); }
+          return up.join('/');
+        })()
+      };
+    }
   });
 })
 
